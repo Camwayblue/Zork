e97b3c949a0855a79a4e342454ce569838e8168c
Camwayblue <justinrover25@gmail.com>
2019-09-11 14:32:59 -0400

    Finding Your Direction

--- Program.cs
+++ Program.cs
@@ -9,20 +9,10 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-            inputString = inputString.ToLower();
-            if (inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork' Lies by the door. ");
-            }
-            else
-            {
-                Console.WriteLine("unreconized command,");
-            }
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
+
         }
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
     }
 }
