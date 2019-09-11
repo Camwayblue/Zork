a594ed9ef1286f0f18433f90fdf3951764c3f5b6
Camwayblue <justinrover25@gmail.com>
2019-09-11 14:00:17 -0400

    2-Looking Around and Quitting
    2-Looking Around and Quitting

--- Program.cs
+++ Program.cs
@@ -7,6 +7,22 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToUpper();
+            inputString = inputString.ToLower();
+            if (inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing.");
+            }
+            else if (inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork' Lies by the door. ");
+            }
+            else
+            {
+                Console.WriteLine("unreconized command,");
+            }
         }
     }
 }
