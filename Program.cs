ab36676eaff644f651430715f431fa7dd9fdd285
Camwayblue <justinrover25@gmail.com>
2019-09-25 23:14:52 -0400

    7-Expanding

--- Program.cs
+++ Program.cs
@@ -3,24 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Zork
 {
     class Program
     {
+
+        private static bool Move(Commands command)
+        {
+            Assert.IsTrue(IsDirection(command), "Invalid Directions.");
+            bool isValidMove = true;
+            switch (command)
+            {
+                case Commands.EAST:
+                    if (Location.Column > 0)
+                    {
+                        Location.Column--;
+                    }
+                    else
+                        isValidMove = false;
+                    break;
+                case Commands.WEST:
+                    if (Location.Column < Rooms.GetLength(1) - 1)
+                    {
+                        Location.Column++;
+                    }
+                    else
+                        isValidMove = false;
+                    break;
+                case Commands.NORTH:
+                    if (Location.Row > 0)
+                    {
+                        Location.Row--;
+                    }
+                    else
+                        isValidMove = false;
+                    break;
+                case Commands.SOUTH:
+                    if (Location.Row < Rooms.GetLength(0) - 1)
+                    {
+                        Location.Row++;
+                    }
+                    else
+                        isValidMove = false;
+                    break;
+                default:
+                    isValidMove = false;
+                    break;
+
+            }
+            return isValidMove;
+        }
         private static Commands Tocommand(string commandstring)
         {
             Commands result;
             return Enum.TryParse(commandstring, true, out result) ? result : Commands.UNKNOWN;
         }
-      
-        private static Commands move(string commandstring)
-        {
-            Commands result;
-            return Enum.TryParse(commandstring, true, out result) ? result == Commands.EAST ? result : result == Commands.WEST ? result : Commands.UNKNOWN : Commands.UNKNOWN;
-        }
 
-        //private static string[] Rooms =  {"Forest","West of House","Behind of House", "Clearing", "Canyon View"};
         private static readonly string[,] Rooms = {
                                                       {"Rocky Trail","South of House","Canyon View"},
                                                       {"Forest","West of House","Behind House"},
@@ -32,6 +72,27 @@ namespace Zork
             public static int Column = 1;
         }
 
+        private static readonly List<Commands> Directions = new List<Commands>
+        {
+            Commands.NORTH,
+            Commands.SOUTH,
+            Commands.EAST,
+            Commands.WEST
+        };
+
+        private static bool IsDirection(Commands command)
+        {
+            return Directions.Contains(command);
+        }
+
+        private static string CurrentRoom
+        {
+            get
+            {
+                return Rooms[Location.Row, Location.Column];
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
@@ -39,57 +100,26 @@ namespace Zork
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
-                Console.WriteLine("Current Room : {0}", Rooms[Location.Row, Location.Column]);
+                string outputstring = "";
+                Console.WriteLine("Current Room : {0}", CurrentRoom);
                 Console.Write(">");
                 command = Tocommand(Console.ReadLine().Trim());
-                string outputstring = "";
                 switch (command)
                 {
+                    case Commands.LOOK:
+                        Console.WriteLine("A Rubber Mat saying 'Welcome to Zork!' liest by the door.");
+                        break;
                     case Commands.EAST:
-                        if (Location.Column > 0)
-                        {
-                            Location.Column--;
-                            outputstring = "You Moved to " + command + ".";
-                        }
-                        else
-                        {
-                            outputstring = "The Way is shut";
-                        }
-                        break;
                     case Commands.WEST:
-                        if (Location.Column < (Rooms.Length / 3) - 1)
-                        {
-                            Location.Column++;
-                            outputstring = "You Moved to " + command + ".";
-                        }
-                        else
-                        {
-                            outputstring = "The Way is Shut";
-                        }
-                        break;
                     case Commands.NORTH:
-                        if (Location.Row > 0)
-                        {
-                            Location.Row--;
-                            outputstring = "You Moved to " + command + ".";
-                        }
-                        else
-                        {
-                            outputstring = "The Way is shut";
-                        }
-                        break;
                     case Commands.SOUTH:
-                        if (Location.Row < (Rooms.Length / 3) - 1)
-                        {
-                            Location.Row++;
-                            outputstring = "You Moved to " + command + ".";
-                        }
-                        else
+                        if (!Move(command))
                         {
                             outputstring = "The Way is Shut";
                         }
                         break;
                     case Commands.QUIT:
+                        Console.Write("Thank you for Playing!");
                         break;
                     default:
                         outputstring = "Unknown Command";
@@ -97,9 +127,7 @@ namespace Zork
                 }
                 Console.WriteLine(outputstring);
             }
-            Console.Write("Thank you for Playing!");
             Console.ReadLine();
-
         }
     }
 }
