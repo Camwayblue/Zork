cbd8b683e85fc021a93ac2bb98cf874aef895e8f
Camwayblue <justinrover25@gmail.com>
2019-09-25 21:36:29 -0400

    5-The Game World

--- Program.cs
+++ Program.cs
@@ -4,52 +4,61 @@ namespace Zork
 {
     class Program
     {
+        private static Commands Tocommand(string commandstring)
+        {
+            Commands result;
+            return Enum.TryParse(commandstring, true, out result) ? result : Commands.UNKNOWN;
+        }
+
+        private static Commands move(string commandstring)
+        {
+            Commands result;
+            return Enum.TryParse(commandstring, true, out result) ? result == Commands.EAST ? result : result == Commands.WEST ? result : Commands.UNKNOWN : Commands.UNKNOWN;
+        }
+
+        private static string[] Rooms = { "Forest", "West of House", "Behind of House", "Clearing", "Canyon View" };
         static void Main(string[] args)
         {
-            string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+            int CurrentRoom = 1;
             Console.WriteLine("Welcome to Zork!");
-            Console.ReadKey();
-
+            Console.WriteLine("Player Spawned in Room : {0}", Rooms[CurrentRoom]);
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
-                Console.Write("> ");
-                command = ToCommand(Console.ReadLine().Trim());
-
-                string outputString;
-
+                Console.WriteLine("Current Room : {0}", Rooms[CurrentRoom]);
+                Console.Write(">");
+                command = move(Console.ReadLine().Trim());
+                string outputstring;
                 switch (command)
                 {
-                    case Commands.QUIT:
-                        outputString = "Thank you for playing!";
-                        break;
-                    case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door.";
-                        break;
-                    case Commands.NORTH:
-                 
-                    case Commands.SOUTH:
-
                     case Commands.EAST:
-
-                    case Commands.WEST:
-                        outputString = $"You moved {command}."
+                        if (CurrentRoom > 0)
+                        {
+                            CurrentRoom--;
+                            outputstring = "You Moved to " + command + ".";
+                        }
+                        else
+                        {
+                            outputstring = "You cannot move any further East";
+                        }
                         break;
-
+                    case Commands.WEST:
+                        if (CurrentRoom < Rooms.Length - 1)
+                        {
+                            CurrentRoom++;
+                            outputstring = "You Moved to " + command + ".";
+                        }
+                        else
+                        {
+                            outputstring = "The Way is Shut";
+                        }
+                        break;
                     default:
-                        outputString = "Unkown command";
+                        outputstring = "Unknown Command";
                         break;
                 }
-
-             //   Console.WriteLine(outputString);
+                Console.WriteLine(outputstring);
             }
-
-
         }
-
-        private static Commands ToCommand(string commandString) => (Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
-
-           
-        
     }
 }
