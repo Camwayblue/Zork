d0494bef55fc6b2ca009382b32439746e8452b91
Camwayblue <justinrover25@gmail.com>
2019-09-26 07:40:22 -0400

    8-Object Oriented Rooms

--- Program.cs
+++ Program.cs
@@ -10,6 +10,91 @@ namespace Zork
     class Program
     {
 
+        #region Data Members
+
+        public class Room
+        {
+            private string mName;
+            private string mDescription;
+            public string Name
+            {
+                get
+                {
+                    return mName;
+                }
+                set
+                {
+                    mName = value;
+                }
+            }
+            public string Description
+            {
+                get
+                {
+                    return mDescription;
+                }
+                set
+                {
+                    mDescription = value;
+                }
+            }
+            public Room(string name, String description = "")
+            {
+                Name = name;
+                Description = description;
+            }
+        }
+
+        private static readonly Room[,] Rooms = {
+                                                      { new Room("Rocky Trail"),new Room("South of House"), new Room("Canyon View")},
+                                                      {new Room("Forest"),new Room("West of House"),new Room("Behind House")},
+                                                      {new Room("Dense Woods"),new Room("North of House"),new Room("Clearing")},
+                                                  };
+
+        private static void InitializationDescription()
+        {
+            Rooms[0, 0].Description = "You are on a Rock-stream Trail.";
+            Rooms[0, 1].Description = "Your are facing the south side of the white house. There is no door here, and all the windows are barred.";
+            Rooms[0, 2].Description = "You are at the top of the Great canyon on its south wall.";
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all direction around you.";
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
+
+            Rooms[2, 0].Description = "This is a daily lit forest, with large trees all around. To the east, there apprears to be sunlight.";
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[2, 2].Description = "You are in a clearing, with a fores surrounding you on the west and south.";
+        }
+        private static class Location
+        {
+            public static int Row = 1;
+            public static int Column = 1;
+        }
+
+        private static readonly List<Commands> Directions = new List<Commands>
+        {
+            Commands.NORTH,
+            Commands.SOUTH,
+            Commands.EAST,
+            Commands.WEST
+        };
+
+
+        private static Room CurrentRoom
+        {
+            get
+            {
+                return Rooms[Location.Row, Location.Column];
+            }
+        }
+        #endregion
+
+        #region Functions
+        private static bool IsDirection(Commands command)
+        {
+            return Directions.Contains(command);
+        }
+
         private static bool Move(Commands command)
         {
             Assert.IsTrue(IsDirection(command), "Invalid Directions.");
@@ -60,54 +145,26 @@ namespace Zork
             Commands result;
             return Enum.TryParse(commandstring, true, out result) ? result : Commands.UNKNOWN;
         }
+        #endregion
 
-        private static readonly string[,] Rooms = {
-                                                      {"Rocky Trail","South of House","Canyon View"},
-                                                      {"Forest","West of House","Behind House"},
-                                                      {"Dense Woods","North of House","Clearing"},
-                                                  };
-        private static class Location
-        {
-            public static int Row = 1;
-            public static int Column = 1;
-        }
-
-        private static readonly List<Commands> Directions = new List<Commands>
-        {
-            Commands.NORTH,
-            Commands.SOUTH,
-            Commands.EAST,
-            Commands.WEST
-        };
-
-        private static bool IsDirection(Commands command)
-        {
-            return Directions.Contains(command);
-        }
-
-        private static string CurrentRoom
-        {
-            get
-            {
-                return Rooms[Location.Row, Location.Column];
-            }
-        }
+        #region Main Method
 
         static void Main(string[] args)
         {
+            InitializationDescription();
             Console.WriteLine("Welcome to Zork!");
-            Console.WriteLine("Player Spawned in Room : {0}", Rooms[Location.Row, Location.Column]);
+            Console.WriteLine("Player Spawned in Room : {0}", CurrentRoom.Name);
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
                 string outputstring = "";
-                Console.WriteLine("Current Room : {0}", CurrentRoom);
+                Console.WriteLine("Current Room : {0}", CurrentRoom.Name);
                 Console.Write(">");
                 command = Tocommand(Console.ReadLine().Trim());
                 switch (command)
                 {
                     case Commands.LOOK:
-                        Console.WriteLine("A Rubber Mat saying 'Welcome to Zork!' liest by the door.");
+                        Console.WriteLine(CurrentRoom.Description);
                         break;
                     case Commands.EAST:
                     case Commands.WEST:
@@ -129,5 +186,6 @@ namespace Zork
             }
             Console.ReadLine();
         }
+        #endregion
     }
 }
